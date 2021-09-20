2d0b61f2e849ef83f4d2b66e300f1f36a60f466a
jassmin-herr <jassminfernandez@gmail.com>
2021-09-20 11:59:55 -0500

    adds challenge

--- js/script.js
+++ js/script.js
@@ -1,26 +1,18 @@
+//Missing number Challenge Arrow Function. 
+
+const missingNumber = (nums) => {
 
 
-function reverse(str){
-    if(!str || typeof str != 'string' || str.length < 2 ) return str;
-    
-    const backwards = [];
-    const totalItems = str.length - 1;
-    for(let i = totalItems; i >= 0; i--){
-      backwards.push(str[i]);
-    }
-    console.log(backwards);
-    return backwards.join('');
-  }
-  
+	if(nums.length === 0) return 0;
+	
+	result = 0;
 
-  console.log(reverse('Hi my name is Jassmin'));
+	for (let i=0; i< nums.length; i++) {
+		result = nums[i] - i + result;
+	}
+	return nums.length - result;
+};
 
-  function mixedNumbers(arr) {
-    // Only change code below this line
-  arr.unshift('I', 2, 'three');//begging of array
-  arr.push(7, 'VIII', 9); //push at the end of array 
-  // Only change code above this line
-    return arr;
-  }
-  
-  console.log(mixedNumbers(['IV', 5, 'six']));
+console.log(missingNumber([3,0,1]));
+
+
